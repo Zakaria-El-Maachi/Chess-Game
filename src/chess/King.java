2db686f50882548a7edf8e0e70c37b71b2891142
imanerh <65502022+imanerh@users.noreply.github.com>
2022-12-02 17:48:33 +0100

    Imane 2

--- src/chess/King.java
+++ src/chess/King.java
@@ -1,18 +1,26 @@
 package chess;
-
+import java.lang.Math;  
 import abstractClasses.Square;
 
 public class King extends ChessPiece{
 
 	public King(String color, int[] coords) {
 		super(color, coords);
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected boolean validMove(Square start, Square end) {
+		// TODO Auto-generated method stub
 		boolean result = false;
 		if(end.getContent()==null || end.getContent().getColor() != this.getColor()) {
-			if(Math.abs(end.getCoords()[0] - start.getCoords()[0]) == 1 || Math.abs(end.getCoords()[1] - start.getCoords()[1]) == 1) {
+			if(Math.abs(end.getCoords()[0] - start.getCoords()[0]) == 1 && Math.abs(end.getCoords()[1] - start.getCoords()[1]) == 1) {
+				result = true;
+			}
+			else if(Math.abs(end.getCoords()[0] - start.getCoords()[0]) == 1 && Math.abs(end.getCoords()[1] - start.getCoords()[1]) == 0){
+				result = true;
+			}
+			else if(Math.abs(end.getCoords()[0] - start.getCoords()[0]) == 0 && Math.abs(end.getCoords()[1] - start.getCoords()[1]) == 1){
 				result = true;
 			}
 		}
--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -6,13 +6,18 @@ public class Knight extends ChessPiece{
 
 	public Knight(String color, int[] coords) {
 		super(color, coords);
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected boolean validMove(Square start, Square end) {
+		// TODO Auto-generated method stub
 		boolean result = false;
 		if(end.getContent()==null || end.getContent().getColor() != this.getColor()) {
 			if(Math.abs(end.getCoords()[0] - start.getCoords()[0]) == 1 && Math.abs(end.getCoords()[1] - start.getCoords()[1]) == 2) {
+				result = true;
+			}
+			else if(Math.abs(end.getCoords()[0] - start.getCoords()[0]) == 2 && Math.abs(end.getCoords()[1] - start.getCoords()[1]) == 1) {
 				result = true;
 			}
 		}
